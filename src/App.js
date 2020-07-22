ac871cb6a5a1d50e54c5aaf8ae009567f1eaf21e
Aneta Szynal <anetaszynal28@gmail.com>
2020-07-22 12:43:32 +0200

    refactor(html): create new component for table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table from "./table";
 
 function App() {
   return (
@@ -33,46 +34,8 @@ function App() {
             <button className="button">Przelicz</button>
           </fieldset>
         </form>
-        <div className="main__tableContainer">
-          <table className="exchangeTable">
-            <caption className="exchangeTable__caption">Kursy stosowane w naszym kantorze</caption>
-            <tr>
-              <th scope="col"></th>
-              <th className="exchangeTable__cell exchangeTable__cell--header" scope="col">PLN</th>
-              <th className="exchangeTable__cell exchangeTable__cell--header" scope="col">EUR</th>
-              <th className="exchangeTable__cell exchangeTable__cell--header" scope="col">GBP</th>
-              <th className="exchangeTable__cell exchangeTable__cell--header" scope="col">USD</th>
-            </tr>
-            <tr className="exchangeTable__row">
-              <th className="exchangeTable__cell exchangeTable__cell--header" scope="row">PLN</th>
-              <td className="exchangeTable__cell">1</td>
-              <td className="exchangeTable__cell">0.2245</td>
-              <td className="exchangeTable__cell">0.2018</td>
-              <td className="exchangeTable__cell">0.2526</td>
-            </tr>
-            <tr className="exchangeTable__row">
-              <th className="exchangeTable__cell exchangeTable__cell--header" scope="row">EUR</th>
-              <td className="exchangeTable__cell">4.4534</td>
-              <td className="exchangeTable__cell">1</td>
-              <td className="exchangeTable__cell">0.8988</td>
-              <td className="exchangeTable__cell">1.1249</td>
-            </tr>
-            <tr className="exchangeTable__row">
-              <th className="exchangeTable__cell exchangeTable__cell--header" scope="row">GBP</th>
-              <td className="exchangeTable__cell">4.9551</td>
-              <td className="exchangeTable__cell">1.1127</td>
-              <td className="exchangeTable__cell">1</td>
-              <td className="exchangeTable__cell">1.2516</td>
-            </tr>
-            <tr className="exchangeTable__row">
-              <th className="exchangeTable__cell exchangeTable__cell--header" scope="row">USD</th>
-              <td className="exchangeTable__cell">3.9589</td>
-              <td className="exchangeTable__cell">0.8890</td>
-              <td className="exchangeTable__cell">0.7990</td>
-              <td className="exchangeTable__cell">1</td>
-            </tr>
-          </table>
-        </div>
+        <Table>
+        </Table>
         <button className="button">Ukryj tabelę z kursami wymiany</button>
         <button className="button">Zmień tło</button>
       </main>
