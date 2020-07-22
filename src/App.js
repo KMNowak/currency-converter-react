871abf1871dd99540e469463eeae2fe2ae6e64c3
Aneta Szynal <anetaszynal28@gmail.com>
2020-07-22 17:06:38 +0200

    feat: add new component for container of HTML

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Table from "./table";
 import Form from "./form";
 import Buttons from "./Buttons";
+import Container from "./container";
 
 function App() {
   return (
-    <main className="main">
+    <Container>
       <Form></Form>
       <Table></Table>
       <Buttons></Buttons>
-    </main>
+    </Container>
   );
 }
 
