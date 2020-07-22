024f74d1a72aff2da5ec0434bdab7c932ce4fbb0
Aneta Szynal <anetaszynal28@gmail.com>
2020-07-22 12:13:28 +0200

    feat(html): initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main className="main">
+        <form className="form">
+          <fieldset className="form__fieldset">
+            <legend className="form__legend">Internetowy Kantor</legend>
+            <p className="form__paragraph form__paragraph--instruction"> Pola, które oznaczone zostały "*" są wymagane.
+                </p>
+            <label className="form__label">Podaj kwotę, którą dysponujesz *:
+                    <input className="form__input" type="number" step="0.01" min="0.01" placeholder="Tutaj wpisz kwotę" required />
+            </label>
+            <label className="form__label">Wybierz walutę, którą posiadasz:
+                    <select className="form__select" name="currency">
+                <option value="EUR">EUR</option>
+                <option value="GBP">GBP</option>
+                <option value="PLN" selected>PLN</option>
+                <option value="USD">USD</option>
+              </select>
+            </label>
+            <label className="form__label">Wybierz walutę, którą chcesz zakupić:
+                    <select className="form__select" name="currency">
+                <option value="EUR" selected>EUR</option>
+                <option value="GBP">GBP</option>
+                <option value="PLN">PLN</option>
+                <option value="USD">USD</option>
+              </select>
+            </label>
+            <p className="form__paragraph"></p>
+
+            <button className="button">Przelicz</button>
+          </fieldset>
+        </form>
+        <div className="main__tableContainer">
+          <table className="exchangeTable">
+            <caption className="exchangeTable__caption">Kursy stosowane w naszym kantorze</caption>
+            <tr>
+              <th scope="col"></th>
+              <th className="exchangeTable__cell exchangeTable__cell--header" scope="col">PLN</th>
+              <th className="exchangeTable__cell exchangeTable__cell--header" scope="col">EUR</th>
+              <th className="exchangeTable__cell exchangeTable__cell--header" scope="col">GBP</th>
+              <th className="exchangeTable__cell exchangeTable__cell--header" scope="col">USD</th>
+            </tr>
+            <tr className="exchangeTable__row">
+              <th className="exchangeTable__cell exchangeTable__cell--header" scope="row">PLN</th>
+              <td className="exchangeTable__cell">1</td>
+              <td className="exchangeTable__cell">0.2245</td>
+              <td className="exchangeTable__cell">0.2018</td>
+              <td className="exchangeTable__cell">0.2526</td>
+            </tr>
+            <tr className="exchangeTable__row">
+              <th className="exchangeTable__cell exchangeTable__cell--header" scope="row">EUR</th>
+              <td className="exchangeTable__cell">4.4534</td>
+              <td className="exchangeTable__cell">1</td>
+              <td className="exchangeTable__cell">0.8988</td>
+              <td className="exchangeTable__cell">1.1249</td>
+            </tr>
+            <tr className="exchangeTable__row">
+              <th className="exchangeTable__cell exchangeTable__cell--header" scope="row">GBP</th>
+              <td className="exchangeTable__cell">4.9551</td>
+              <td className="exchangeTable__cell">1.1127</td>
+              <td className="exchangeTable__cell">1</td>
+              <td className="exchangeTable__cell">1.2516</td>
+            </tr>
+            <tr className="exchangeTable__row">
+              <th className="exchangeTable__cell exchangeTable__cell--header" scope="row">USD</th>
+              <td className="exchangeTable__cell">3.9589</td>
+              <td className="exchangeTable__cell">0.8890</td>
+              <td className="exchangeTable__cell">0.7990</td>
+              <td className="exchangeTable__cell">1</td>
+            </tr>
+          </table>
+        </div>
+        <button className="button">Ukryj tabelę z kursami wymiany</button>
+        <button className="button">Zmień tło</button>
+      </main>
     </div>
   );
 }
